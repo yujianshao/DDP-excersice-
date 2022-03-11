095d58fa7330776629adf13f9705d99e258117b3
yujianshao <101124333+yujianshao@users.noreply.github.com>
2022-03-11 14:45:24 +0000

    Add files via upload

---
+++ Question 3 Yujian Shao.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Question 3
+world = input('Write down your world:')
+
+
+# In[ ]:
+
+
+lowercase=0
+uppercase = 0
+for i in world:
+    if i.islower():
+        lowercase=lowercase+1
+    elif i.isupper():
+        uppercase=uppercase+1
+
+
+# In[ ]:
+
+
+print (f'Hi,your world has {lowercase} lowercases, and {uppercase} uppercases!')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
